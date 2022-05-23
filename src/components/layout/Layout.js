3305c705b64d7a97d9bcbe090dbba5ab8b837f79
Jose Chimeno Espinola <josechu@MacBook-Pro-de-Jose.local>
2022-05-23 18:54:04 +0200

    Show and hide menu when page is scrolling

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,10 @@
 
-import classes from './Layout.module.css';
+import classes from './Layout.module.css'
 
-export default function Layout({children}) {
+export default function Layout ({ children }) {
   return (
     <div>
       <main className={classes.main}>{children}</main>
     </div>
-  );
+  )
 }
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,32 +1,54 @@
-import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/constants";
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Collapse } from '@mui/material'
 
-import classes from "./MainNavigation.module.css";
+import classes from './MainNavigation.module.css'
+import { getNumOfFav } from '../../util-hooks/localStorage'
 
-export default function MainNavigation({ setPage }) {
+export default function MainNavigation () {
+  const [collapse, setCollapse] = useState(true)
+  let prevScrollpos = window.pageYOffset
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollCollapseMenu, true)
+    getNumOfFav()
+  }, [])
+
+  /**
+   * Function that allows to check if the actual position of scrollbar is in movement,
+   * and show or hide menu depending of his position
+   *  */
+  function scrollCollapseMenu () {
+    const currentScrollPos = window.pageYOffset
+    if (document.documentElement.scrollTop > 25) {
+      prevScrollpos > currentScrollPos ? setCollapse(true) : setCollapse(false)
+      prevScrollpos = currentScrollPos
+    } else {
+      setCollapse(true)
+    }
+  }
+
   return (
-    <header className={classes.header} data-test="navigation-header">
-      <div className={classes.logo}>React Meetups</div>
-      <nav>
-        <ul>
-          <li>
-            <a href="#" onClick={() => setPage(ALL_MEETUP_PAGE)}>
-              All Meetups
-            </a>
-          </li>
-
-          <li>
-            <a href="#" onClick={() => setPage(NEW_MEETUP_PAGE)}>
-              Add New Meetup
-            </a>
-          </li>
-          <li>
-            <a href="#" onClick={() => setPage(FAVORITES_PAGE)}>
-              My Favorites
-              <span className={classes.badge}>{0}</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    <Collapse in={collapse} collapsedSize={0} className={classes.collapseheader}>
+      <header id="header-container" data-test="navigation-header" className={classes.header}>
+        <div className={classes.logo}>React Meetups</div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All Meetups</Link>
+            </li>
+            <li>
+              <Link to="/newmeetup">Add New Meetup</Link>
+            </li>
+            <li>
+              <Link to="/favorites">
+                My Favorites
+                <span className={classes.badge} id="num-favs-counter">0</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </Collapse>
+  )
 }
